6f81679987e700331a870c5c19be3255e1bfa5b4
allamanamedearl <lablover_4@hotmail.com>
2018-05-05 12:39:40 -0400

    -set up animation for player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,10 +7,22 @@ public class PlayerMovement : MonoBehaviour {
     public float Speed;
     public float JumpSpeed;
     private bool isJumping;
+
+    //Animation stuff
+    const int STATE_IDLE = 0;
+    const int STATE_RUN = 1;
+    const int STATE_JUMP = 2;
+    const int STATE_DEATH = 3;
+
+    private int currentAnimState;
+    private string currDirection;
+
 	// Use this for initialization
 	void Start ()
     {
         isJumping = false;
+        currentAnimState = STATE_IDLE;
+        currDirection = "left";
 	}
 	
 	// Update is called once per frame
@@ -18,24 +30,80 @@ public class PlayerMovement : MonoBehaviour {
     {
         Vector3 direction = new Vector3(0, 0, 0);
         direction.x = Input.GetAxis("Horizontal");
-		//if(Input.GetKey(KeyCode.LeftArrow))
-  //      {
-  //          direction.x = -1.0f;
-  //      }
-  //      if (Input.GetKey(KeyCode.RightArrow))
-  //      {
-  //          direction.x = 1.0f;
-  //      }
-        if (/*Input.GetKeyDown(KeyCode.Space) || */Input.GetButtonDown("Jump") &&!isJumping)
+
+        if (Input.GetButtonDown("Jump") &&!isJumping)
         {
             //direction.y = 1.0f;
             GetComponent<Rigidbody2D>().AddForce(new Vector2(0, JumpSpeed),ForceMode2D.Impulse);
             isJumping = true;
+            ChangeAnimState(STATE_JUMP);
+        }
+
+        if(!isJumping)
+        {
+            if (direction == Vector3.zero)
+            {
+                ChangeAnimState(STATE_IDLE);
+            }
+            else
+            {
+                if (direction.x < 0)//left
+                {
+                    ChangeDirection("left");           
+                }
+                else
+                {
+                    ChangeDirection("right");
+                }
+
+                ChangeAnimState(STATE_RUN);
+            }
+            
         }
         
-        transform.Translate(direction * Speed * Time.deltaTime);
+        
+        transform.position+=(direction * Speed * Time.deltaTime);
         
         
+    }
+    private void ChangeAnimState(int state)
+    {
+        if (currentAnimState == state)
+            return;
+        Animator anim = GetComponent<Animator>();
+        Debug.Assert(anim != null);
+        if(anim!=null)
+        {
+            anim.SetInteger("state", state);
+            currentAnimState = state;
+            Debug.Log("State is " + state);
+        }
+        //switch(state)
+        //{
+        //    case STATE_IDLE:
+        //        break;
+        //    case STATE_RUN:
+        //        break;
+        //    case STATE_JUMP:
+        //        break;
+        //    case STATE_DEATH:
+        //        break;
+        //}
+    }
+
+    private void ChangeDirection(string direction)
+    {
+        if (currDirection == direction)
+            return;
+        if(direction=="left")
+        {
+            transform.Rotate(0, 180, 0);
+        }
+        else
+        {
+            transform.Rotate(0, -180, 0);
+        }
+        currDirection = direction;
     }
 
     public void OnCollisionEnter2D(Collision2D c)
